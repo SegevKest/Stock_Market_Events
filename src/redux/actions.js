cb762f17d34fcbd66a98966d31fc6c84a9977825
Segev <Segev.k@ez-roi.com>
2020-11-08 19:49:15 +0200

    reducers and actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,3 +1,11 @@
+
+// Login + Logout
 export const logIn = userObj => ({type:"USER_LOGIN",data:userObj});//all users
 export const logOut=()=>({type:"USER_LOGOUT"});//all users
 
+
+//Fetch Data from API's
+export const getAllNews = allNewsObj => ({type : 'USER_FETCH_NEWS', data : allNewsObj});
+
+export const getAlliPos = iPosObj => ({type : 'USER_FETCH_IPOS', data : iPosObj});
+export const getAllEarnings = earningsObj => ({type : 'USER_FETCH_EARNINGS', data : earningsObj});
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-unused-vars */
 const initialLocalState = {
 
 /** The initialized State for the website - will include all the iPos, Earining  news and customerData */
                                 allNews:[],
                                 iPos:[],
-                                Earining:[],
-                                customer:
+                                earinings:[],
+                                user:
                                 {
                                         firstName:'',
                                         lastName:'',
@@ -17,18 +18,45 @@ const initialLocalState = {
 
 const stockEventsReducer = (state = initialLocalState , action) => {
 
+        let newState, dataFromLocalStorage;
+        newState = Object.assign({}, state);
+
         switch (action.type) {
+
                 // For Login
+                // {type:"USER_LOGIN", data:userObj}
                 case 'USER_LOGIN':
+                        newState.user = action.data; 
+                        return newState;
+
+                // For Logout
+                // {type:"USER_LOGOUT"}
                 case 'USER_LOGOUT':
+                        newState.user = initialLocalState.user;
+                        return newState;
+                
+                //ForFetching the News From API
+                // {type:"USER_FETCH_NEWS", data:newArray_FromAPI}
                 case 'USER_FETCH_NEWS':
+                        newState.allNews = action.data;  
+                        return newState;
+
+                //ForFetching the IPOS From API
+                // {type:"USER_FETCH_IPOS", data:newArray_FromIPOS}
                 case 'USER_FETCH_IPOS':
+                        newState.iPos = action.data;     
+                        return newState;
+
+                //ForFetching the EARNINGS From API
+                // {type:"USER_FETCH_EARNINGS", data:newArray_FromEARNINGS}
                 case 'USER_FETCH_EARNINGS':
+                        newState.earinings = action.data;     
+                        return newState;
 
                 default:
                         return state;
         }
 };
 
-export default StockEventsReducer;
+export default stockEventsReducer;
 
